ccc2f63d9b8e075ac01f9b43187c5835ec477574
djongo <jonasholstein@gmail.com>
2010-08-21 22:46:19 +0200

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '391738acaa540edfefac476ec6eaaf7f1d91cf8f358daf2a38b798b07e79abe22626c9981a8602e4bed5f02207c21ae5d9e203d1d1ab07124edb8d73ef3d96ea';
