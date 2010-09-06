f708fa552f4c45323a89e6b8fec2442815e4fd2f
djongo <jonasholstein@gmail.com>
2010-09-06 09:56:04 +0200

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                     "Jonas Holstein"
+  user.email                    "jonasholstein@gmail.com"
+  user.password                 "foobar"
+  user.password_confirmation    "foobar"
+end
+  
